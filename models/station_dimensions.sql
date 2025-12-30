bccf2e661ab4c664fd4aa8fde719b7cb20132cfe
Vishnu Pachauri <vishnupachaurilearning@gmail.com>
2025-12-30 08:10:04 +0000

    correction of data 2018

--- models/station_dimensions.sql
+++ models/station_dimensions.sql
@@ -2,15 +2,14 @@ with bike as (
 
 select 
 distinct
-START_STATIO_ID AS station_id,
+START_STATION_ID AS station_id,
 START_STATION_NAME as station_name,
 START_LAT as station_lat ,
 START_LNG  as station_lng
 
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 where RIDE_ID != 'ride_id'
 
-limit 10
 )
 
 select * 
--- models/trip_fact.sql
+++ models/trip_fact.sql
@@ -2,17 +2,17 @@ with bike as (
 
 select 
 RIDE_ID,
-RIDEABLE_TYPE,
+-- RIDEABLE_TYPE,
 Date(TO_TIMESTAMP(started_at)) as trip_date,
 --DATE(TO_TIMESTAMP(ENDED_AT)) as trip_end_date,
-START_STATIO_ID as station_id,
+START_STATION_ID as station_id,
 END_STATION_ID as end_station_id,
 MEMBER_CSUAL as member_casual,
 --DATE(TO_TIMESTAMP(ENDED_AT)) - DATE(TO_TIMESTAMP(started_at)) as trip_duartion,
 TIMESTAMPDIFF(SECOND, TO_TIMESTAMP(started_at), TO_TIMESTAMP(ENDED_AT)) as trip_duartion_seconds
 
 
-from  {{ source('demo', 'bike') }}
+from  {{ ref('stg_bike') }}
 
 where RIDE_ID != 'ride_id'
 
